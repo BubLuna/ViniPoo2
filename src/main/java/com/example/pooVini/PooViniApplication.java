484738a19e9239270c894638fb8e2d2920ee127f
BubLuna <rebecalunardibublitz@gmail.com>
2026-06-23 15:05:45 -0300

    Spring Initializer
    comit do spring initializer
    adicionei exatamente o que foi falado nos slides do "spring"

---
+++ src/main/java/com/example/pooVini/PooViniApplication.java
@@ -0,0 +1,13 @@
+package com.example.pooVini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooViniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooViniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pooVini/PooViniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pooVini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooViniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
